291e7f088cc707a45127ef24d79b7960584eb3bd
QuSijin <775607737@qq.com>
2020-07-18 16:16:35 +0800

    链表中倒数第k个节点2020/7/18

--- 链表中倒数第k个节点.cpp
+++ 链表中倒数第k个节点.cpp
@@ -1,3 +1,4 @@
+//第一次复习
 //经典快慢指针
 class Solution {
 public:
@@ -17,4 +18,33 @@ public:
         }
         return low;
     }
-i};
+};
+
+//第二次复习
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* getKthFromEnd(ListNode* head, int k) {
+        ListNode* fast = head;
+        ListNode* low = head;
+        while(fast!=NULL)
+        {
+            fast = fast->next;
+            if(k==0)
+            {
+                low = low->next;
+            }else{
+                k--;
+            }
+        }     
+        return low;
+    }
+};
