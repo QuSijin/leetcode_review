a8024af8f40589c2d1e201284ae79295a38b73de
QuSijin <775607737@qq.com>
2020-07-18 21:56:26 +0800

    三数之和2020/7/18复习第一次

---
+++ 三数之和.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+       sort(nums.begin(),nums.end());
+       vector<vector<int>> res;
+       for(int k=0;k<int (nums.size()-2) && nums[k]<=0;k++)
+       {
+           int l = k+1;
+           int r = nums.size()-1;
+           if(k>0 && nums[k]==nums[k-1]) continue;
+           
+           while(l<r)
+           {
+               int sum = nums[k]+nums[l]+nums[r];
+               if(sum<0) {while(l<r&&nums[l]==nums[++l]);}
+               else if(sum>0) {while(l<r&&nums[r]==nums[--r]);}
+               else {
+                   res.push_back({nums[l],nums[k],nums[r]});
+                   while(l<r&&nums[l]==nums[++l]);
+                   while(l<r&&nums[r]==nums[--r]);
+               }
+           }
+       }
+       return res;
+       
+    }
+};
+int main()
+{
+    Solution so;
+    vector<int> a = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> b = so.threeSum(a);
+    cout << b[0][0] <<'\t';
+    cout << b[0][1]<<'\t';
+    cout << b[0][2]<<'\t';
+    cout << b[1][0]<<'\t';
+    cout << b[1][1]<<'\t';
+    cout << b[1][2]<<'\t';
+    system("pause");
+    return 0;
+}
