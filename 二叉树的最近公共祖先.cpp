5e2d6025ead032f7389346cdfcc86ae8d72461fd
QuSijin <775607737@qq.com>
2020-07-29 15:52:32 +0800

    二叉树的最近公共祖先2020/7/29复习第二次

--- 二叉树的最近公共祖先.cpp
+++ 二叉树的最近公共祖先.cpp
@@ -1,3 +1,4 @@
+//1.
 class Solution {
 public:
     TreeNode* ans;
@@ -14,5 +15,28 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         dfs(root, p, q);
         return ans;
+    }
+};
+//2.更容易接受理解的解法
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root ==nullptr|| root == p||root==q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        if(left==nullptr) return right;
+        if(right==nullptr) return left;
+        if(left==nullptr && right==nullptr) return nullptr;
+        return root;
     }
 };
