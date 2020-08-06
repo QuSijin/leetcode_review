b3424e41282147065ba6f131be1bbf25b45537e2
QuSijin <775607737@qq.com>
2020-08-06 16:41:00 +0800

    复杂链表的复制2020/8/6复习第一次两种解法

--- 复杂链表的复制.cpp
+++ 复杂链表的复制.cpp
@@ -24,3 +24,52 @@ public:
        return mp[head];
     }
 };
+//2.原地修改链表
+///*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        // 完成链表节点的复制
+        Node* cur = head;
+        while(cur != nullptr){
+            Node* tmp = new Node(cur->val);
+            tmp->next = cur->next;
+            cur->next = tmp;
+            cur = cur->next->next;//下一个变成当前节点
+        }
+       // 完成链表复制节点的随机指针复制
+        cur = head;
+        while(cur != nullptr){
+            if(cur->random != nullptr)// 注意判断原来的节点有没有random指针
+                cur->next->random = cur->random->next;//若1random->3,1'random->3'
+            cur = cur->next->next;
+        }
+         // 将链表一分为二
+       Node* copyHead = head->next;
+        cur = head;
+        Node* curCopy = head->next;
+        while (cur != nullptr) {
+            cur->next = cur->next->next;
+            cur = cur->next;
+            if (curCopy->next != nullptr) {
+                curCopy->next = curCopy->next->next;
+                curCopy = curCopy->next;
+            }
+        }
+        return copyHead;
+    }
+};
